5f535489fa32c4619c3664ab0726db83ae35700c
Etienne Bourganel <etienne.bourganel.com>
2020-08-31 17:45:33 +0200

    Add basic logic for operators to run from web

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Operation functions */
+
 function add(a,b){
     return a+b;
 };
@@ -28,3 +30,53 @@ function operate (operator,a,b){
         return divide(a,b);
     };
 };
+
+//Functions
+
+let display = document.getElementById('display');
+
+let inputArray = [];
+let displayValue = '_';
+let a;
+let b;
+let operator;
+let result;
+
+display.innerHTML = displayValue;
+
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach(button => button.addEventListener('click', inputNumber));
+
+function inputNumber (e){
+    inputArray.push(e.target.value);
+    displayValue = inputArray.join('');
+    display.innerHTML = displayValue;
+}
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click',clearValues);
+
+function clearValues(){
+    inputArray = [];
+    displayValue='_';
+    display.innerHTML = displayValue;
+}
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(button =>button.addEventListener('click',setOperator));
+
+function setOperator(e){
+    a=parseFloat(displayValue);
+    operator=e.target.value;
+    clearValues();
+}
+
+const equalsButton = document.getElementById('equal');
+equalsButton.addEventListener('click',preOperate);
+
+function preOperate(){
+    b=parseFloat(displayValue);
+    result = operate(operator,a,b)
+    console.log(result);
+    display.innerHTML= result;
+}
