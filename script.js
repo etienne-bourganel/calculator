633540779aa38c456345d29f7ee5621fb58d2655
Etienne Bourganel <etienne.bourganel.com>
2020-08-31 17:55:09 +0200

    Add comments to script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Operation functions */
+/* Basic operation functions */
 
 function add(a,b){
     return a+b;
@@ -31,7 +31,7 @@ function operate (operator,a,b){
     };
 };
 
-//Functions
+//Variables initialization
 
 let display = document.getElementById('display');
 
@@ -44,6 +44,8 @@ let result;
 
 display.innerHTML = displayValue;
 
+//Add the value of a button to the inputArray and display the corresponding string
+
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach(button => button.addEventListener('click', inputNumber));
 
@@ -53,23 +55,32 @@ function inputNumber (e){
     display.innerHTML = displayValue;
 }
 
+//Defines the Clear function by erasing display and unset a & b
+
 const clear = document.getElementById('clear');
 clear.addEventListener('click',clearValues);
 
 function clearValues(){
     inputArray = [];
     displayValue='_';
+    a = undefined;
+    b = undefined;
     display.innerHTML = displayValue;
 }
+
+//Defines operators from buttons and set corresponding operator value
 
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(button =>button.addEventListener('click',setOperator));
 
 function setOperator(e){
+    display.innerHTML = '_';
+    inputArray = [];
     a=parseFloat(displayValue);
     operator=e.target.value;
-    clearValues();
 }
+
+//Makes the whole calculation based on previous values
 
 const equalsButton = document.getElementById('equal');
 equalsButton.addEventListener('click',preOperate);
