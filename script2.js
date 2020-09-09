741ea9a335fe775b204b368f4e67ae53ea91d675
etienne-bourganel <etienne.bourganel@gmail.com>
2020-09-09 12:59:16 +0200

    Add pseudo-code in script2.js to focus on cleaner code

---
+++ script2.js
@@ -0,0 +1,22 @@
+// Create variables for operations called a, b, result and operator
+
+// Create functions for basic operations called add, substract, multiply and divide
+
+// Create function for calling appropriate operations called operate that return result
+    // If (a,b or operator are undefined), return ERROR
+    // If (b = 0 and operator = '/'), return ERROR
+    // Else return result based on appropriate operation
+
+// Get input from user
+
+    // If input is a number and a or operator are not defined yet, store it in a
+        // Otherwise store input into b
+
+    // If input is an operator, and operator is undefined, store operator value in operator
+        
+        // If (a and operator) are defined or if a is undefined, do nothing
+        // Else if (a, b and operator) are defined, perform function operate and store result in a
+
+    // If input is equal sign, perform operate function based on a, b and operator
+
+//
